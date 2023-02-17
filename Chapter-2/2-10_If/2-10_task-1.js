87d2612885ed133631134871afebf096f20ae793
cgilut <117078690+cgilut@users.noreply.github.com>
2023-02-17 16:44:48 +0100

    Complete first task in chapter 2.10

---
+++ Chapter-2/2-10_If/2-10_task-1.js
@@ -0,0 +1,8 @@
+"use strict"
+
+let officialJavaScriptName = prompt("Какое «официальное» название JavaScript?")
+    if (officialJavaScriptName == "ECMAScript") {
+        alert("Верно!");
+    } else {
+        alert("Hе знаете? ECMAScript!");
+    }
