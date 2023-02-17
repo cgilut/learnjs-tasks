9b52be62969fdc2bcaa177bdc4f0969e23155bc6
cgilut <117078690+cgilut@users.noreply.github.com>
2023-02-17 17:15:55 +0100

    Complete fourth task in chapter 2.10

---
+++ Chapter-2/2-10_If/2-10_task-4.js
@@ -0,0 +1,9 @@
+"use strict"
+
+let login = prompt("Вы Сотрудник или Директор?");
+let message = 
+    (login == "Сотрудник") ? "Привет" :
+    (login == "Директор") ? "Здравствуйте" :
+    (login == "") ? "Нет логина" : "";
+
+alert(message);
