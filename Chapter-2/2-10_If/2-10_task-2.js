6ba4a52a0de10dcc5ba7015fad085e4ba6a522c0
cgilut <117078690+cgilut@users.noreply.github.com>
2023-02-17 16:56:40 +0100

    Complete second task in chapter 2.10

---
+++ Chapter-2/2-10_If/2-10_task-2.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let userInputInteger = prompt("Пожалуйста, введите число");
+
+if (userInputInteger > 0) {
+    alert("1");
+} else if (userInputInteger < 0) {
+    alert("-1");
+} else if (userInputInteger == 0) {
+    alert("0");
+}
