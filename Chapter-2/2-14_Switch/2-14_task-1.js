707b1f14aa55da4d51933caf8e5449e006cf0418
cgilut <117078690+cgilut@users.noreply.github.com>
2023-02-19 13:12:05 +0100

    Change console.log to alert

--- Chapter-2/2-14_Switch/2-14_task-1.js
+++ Chapter-2/2-14_Switch/2-14_task-1.js
@@ -3,12 +3,12 @@
 let browser;
 
   if (browser == "Edge") {
-    console.log( "You've got the Edge!" );
+    alert( "You've got the Edge!" );
   } else if (browser == "Chrome" 
         || browser == "Firefox" 
         || browser == "Safari" 
         || browser == "Opera") {
-    console.log( "Okay we support these browsers too" );
+    alert( "Okay we support these browsers too" );
   } else {
-    console.log( "We hope that this page looks ok!" );
+    alert( "We hope that this page looks ok!" );
   };
